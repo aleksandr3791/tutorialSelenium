aad71822f1d740caa44a720ba1cbef2a0ec14a48
Aleksandra La <aleksandra.latk@gmail.com>
2022-05-10 21:46:02 +0200

    Add weryfikacjaSelektorowWPrzegladarce, changes in FindByCss

--- src/test/java/findElementsBy/FindByCss.java
+++ src/test/java/findElementsBy/FindByCss.java
@@ -1,6 +1,7 @@
 package findElementsBy;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -12,6 +13,24 @@ public class FindByCss {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        By cssId = By.cssSelector("#clickOnMe");
+        driver.findElement(cssId);
+
+        By cssClass = By.cssSelector(".topSecret");
+        driver.findElement(cssClass);
+
+        By cssTag = By.cssSelector("input");
+        driver.findElement(cssTag).sendKeys("Pierwszy");
+
+        By cssName = By.cssSelector("[name = 'fname']");
+        driver.findElement(cssName);
+
+        By cssClass2 = By.cssSelector("[class = 'topSecret]");
+        driver.findElement(cssClass2);
+
+
+
 
 
     }
